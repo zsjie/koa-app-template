540c168042e317f0064e5885a528f30398b81c2a
zhushijie <zhushijie@jd.com>
2021-08-26 10:56:13 +0800

    feat: add access log

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import Koa from 'koa'
+import accesslog from 'koa-accesslog'
 
 import route from '~/lib/route'
 
 export default function(): Koa {
   const app = new Koa()
 
+  app.proxy = true
+
+  app.use(accesslog())
   app.use(route().middleware())
 
   return app
